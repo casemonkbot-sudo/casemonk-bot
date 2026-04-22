ccd42b9c382dacd72a294bbd81e02dd3dd292167
casemonkbot-sudo <case.monk.bot@gmail.com>
2026-04-22 17:31:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const path = require('path');
 
 const bot = new Telegraf('8774206010:AAHmvdYuN0xUOCL8poGlLYuy_SkaV2r9cUg');
 const app = express();
-
-// Добавляем простую сессию, чтобы бот помнил, что ты вводишь пароль
 bot.use(session());
 
 const ADMIN_ID = 999313063;
@@ -14,14 +12,13 @@ const ADMIN_PASSWORD = "9993133063";
 app.use(express.static(path.join(__dirname, 'public')));
 
 bot.start((ctx) => {
-    ctx.reply(`🎁 Добро пожаловать в CASEMONK!`, {
+    ctx.reply(`🎁 Привет! Жми «Играть», чтобы открыть Farm Case!`, {
         reply_markup: {
             inline_keyboard: [[{ text: "Играть 🚀", web_app: { url: "https://" + process.env.RENDER_EXTERNAL_HOSTNAME } }]]
         }
     });
 });
 
-// Секретная команда
 bot.command('manage', (ctx) => {
     if (ctx.from.id === ADMIN_ID) {
         ctx.session = { waitPass: true };
@@ -29,19 +26,15 @@ bot.command('manage', (ctx) => {
     }
 });
 
-// Проверка пароля
 bot.on('text', (ctx) => {
     if (ctx.session?.waitPass) {
         if (ctx.message.text === ADMIN_PASSWORD) {
             ctx.session.waitPass = false;
-            ctx.reply("✅ Доступ разрешен. Открываю админ-панель...", {
+            ctx.reply("✅ Пароль верный. Вход в панель:", {
                 reply_markup: {
-                    inline_keyboard: [[{ text: "⚙️ Войти в Админку", web_app: { url: "https://" + process.env.RENDER_EXTERNAL_HOSTNAME + "/admin.html" } }]]
+                    inline_keyboard: [[{ text: "⚙️ Админка", web_app: { url: "https://" + process.env.RENDER_EXTERNAL_HOSTNAME + "/admin.html" } }]]
                 }
             });
-        } else {
-            ctx.reply("❌ Неверный пароль.");
-            ctx.session.waitPass = false;
         }
     }
 });
